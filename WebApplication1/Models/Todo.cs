3161a8c4917d444f2b3b3a77b5410c52a6988faf
ki212-lme <ki212_lme@student.ztu.edu.ua>
2022-11-17 17:08:45 +0200

    added todo model

---
+++ WebApplication1/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public int Description { get; set; }
+    public DateTime DateComplete { get; set; }
+    public bool? IsCompleted { get; set; }
+}
